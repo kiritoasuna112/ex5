4daea12f9511e478c3f07da82c92871fbe0f9b9e
Potsawat Khunkum <aikahotdeath@gmail.com>
2019-07-20 15:36:07 +0700

    3

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/show', function(req, res, next) {
+  res.render('show');
+});
+
 module.exports = router;
